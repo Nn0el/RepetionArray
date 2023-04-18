c9b6bede7bc15bd8b986a32121fa0fb056e09301
Nn0el <noel.johansson@elev.ga.ntig.se>
2023-04-18 15:11:28 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        Scanner fil;
+        try {
+            fil = new Scanner(new File("ideaold.log"));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        list.add(fil.nextLine());
+System.out.println(list.get(0));
+    }
+}
