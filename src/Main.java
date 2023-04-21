aa031b0a4553fb9886f54ab86573ebb5c51de928
Nn0el <noel.johansson@elev.ga.ntig.se>
2023-04-21 11:09:15 +0200

    Börjat på att skapa olika metoder

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,21 @@ public class Main {
         Scanner fil;
         try {
             fil = new Scanner(new File("ideaold.log"));
-        } catch (FileNotFoundException e) {
+        } catch (
+                FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-        list.add(fil.nextLine());
-System.out.println(list.get(0));
-
+        while (fil.hasNextLine()) {
+            list.add(fil.nextLine());
+        }
     }
+        public static void errorcount {
+        
+        int i = 0 ;
+        for ( ;i < list.size() ; i++) {
+            if (list.get(i).contains("ERROR"))
+                System.out.println(list.get(i));
+        }
+        }
+
 }
